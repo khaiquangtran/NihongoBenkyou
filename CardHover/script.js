8641d133ad11d7162959ff7519355f87d9a54b4d
KhaiTQ10 <khaiquangtranabc@gmail.com>
2023-11-05 17:15:22 +0700

    add file audio

--- CardHover/script.js
+++ CardHover/script.js
@@ -4,6 +4,5 @@ const audio = document.getElementById("audio");
 playBtn.addEventListener("click", function () {
   if (audio.paused) {
     audio.play();
-    alert("Mình chưa thêm âm thanh bạn gì ơi!");
   }
 });
