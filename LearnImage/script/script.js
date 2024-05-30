c497a73edda531f5ae48da69e15ab3d3d392168a
KhaiTQ10 <khaiquangtranabc@gmail.com>
2024-05-30 22:20:43 +0700

    big update LearnImage

--- LearnImage/script/script.js
+++ LearnImage/script/script.js
@@ -1,5 +1,15 @@
 document.querySelectorAll(".card").forEach(function (card) {
-    card.addEventListener("click", function () {
-      this.classList.toggle("card1");
-    });
+  card.addEventListener("click", function () {
+    this.classList.toggle("card1");
   });
+});
+
+var randomChange = document.getElementById('Background');
+images = ['../Background/background.png', '../Background/background1.png', '../Background/background2.jpg', '../Background/background3.jpg',
+    '../Background/background4.jpg', '../Background/background5.jpg'];
+var imgCount = images.length;
+var number = Math.floor(Math.random() * imgCount);
+
+window.onload = function () {
+    randomChange.style.backgroundImage = 'url(' + images[number] + ')';
+}
