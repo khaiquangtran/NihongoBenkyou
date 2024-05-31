33e13f0e0876b3d310519c1b7dcdc3393e75694b
KhaiTQ10 <khaiquangtranabc@gmail.com>
2024-05-31 22:08:01 +0700

    update kanji

---
+++ Kanji/Config/Script/script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll(".card").forEach(function (card) {
+  card.addEventListener("click", function () {
+    this.classList.toggle("card1");
+  });
+});
+
+var randomChange = document.getElementById("Background");
+images = [
+  "../Config/Background/background.jpg",
+  "../Config/Background/background1.jpg",
+  "../Config/Background/background2.jpg",
+  "../Config/Background/background3.jpg",
+  "../Config/Background/background4.jpg",
+  "../Config/Background/background5.jpg",
+  "../Config/Background/background6.jpg",
+];
+var imgCount = images.length;
+var number = Math.floor(Math.random() * imgCount);
+
+window.onload = function () {
+  randomChange.style.backgroundImage = "url(" + images[number] + ")";
+};
--- LearnImage/Config/Script/script.js
+++ LearnImage/Config/Script/script.js
@@ -4,12 +4,18 @@ document.querySelectorAll(".card").forEach(function (card) {
   });
 });
 
-var randomChange = document.getElementById('Background');
-images = ['../Config/Background/background.png', '../Config/Background/background1.png', '../Config/Background/background2.jpg', '../Config/Background/background3.jpg',
-    '../Config/Background/background4.jpg', '../Config/Background/background5.jpg'];
+var randomChange = document.getElementById("Background");
+images = [
+  "../Config/Background/background.png",
+  "../Config/Background/background1.png",
+  "../Config/Background/background2.jpg",
+  "../Config/Background/background3.jpg",
+  "../Config/Background/background4.jpg",
+  "../Config/Background/background5.jpg",
+];
 var imgCount = images.length;
 var number = Math.floor(Math.random() * imgCount);
 
 window.onload = function () {
-    randomChange.style.backgroundImage = 'url(' + images[number] + ')';
-}
+  randomChange.style.backgroundImage = "url(" + images[number] + ")";
+};
