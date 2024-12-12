031d19abc32d6f69bd88e15e47cc30395b2cdbd8
KhaiTQ10 <khaiquangtranabc@gmail.com>
2024-12-13 01:51:00 +0700

    update new feature

--- Home/script.js
+++ Home/script.js
@@ -37,4 +37,58 @@ window.onload = function () {
   randomChange.style.backgroundImage = "url(" + images[number] + ")";
 };
 
-console.log("Home")
+// console.log("Home")
+
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let dd = document.getElementById('dd');
+let hh = document.getElementById('hh');
+let mm = document.getElementById('mm');
+let ss = document.getElementById('ss');
+let day_dot = document.querySelector('.day_dot');
+let hr_dot = document.querySelector('.hr_dot');
+let min_dot = document.querySelector('.min_dot');
+let sec_dot = document.querySelector('.sec_dot');
+let endDate = '1/1/2025 00:00:00';
+//date format i.e., mm/dd/yyyy
+let x = setInterval(function(){
+let now = new Date(endDate).getTime();
+let countDown = new Date().getTime();
+let distance = now - countDown;
+//time calculation for remaining days,hours, seconds
+let d = Math.floor(distance / (1000 * 60 * 60 * 24));
+let h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60 ));
+let s = Math.floor((distance % (1000 * 60 )) / (1000));
+//lets output the result in the elements with id
+days.innerHTML = d + "<br><span>Days</span>";
+hours.innerHTML = h + "<br><span>Hours</span>";
+minutes.innerHTML = m + "<br><span>Minutes</span>";
+seconds.innerHTML = s + "<br><span>Seconds</span>";
+//lets animate stroke
+dd.style.strokeDashoffset = 440 - (440 * d) / 365;
+//365 days in a year
+hh.style.strokeDashoffset = 440 - (440 * h) / 24;
+//there is 24hours in a day
+mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+//60 minutes in an hour
+ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+//60 seconds in a minute
+//lets animate circle dots
+day_dot.style.transform = `rotateZ(${d * 0.986}deg`;
+//360deg / 365 days = 0.986
+hr_dot.style.transform = `rotateZ(${h * 15}deg`;
+//360deg / 24 hrs = 15
+min_dot.style.transform = `rotateZ(${m * 6}deg`;
+//360deg / 60 minutes = 6
+sec_dot.style.transform = `rotateZ(${s * 6}deg`;
+//360deg / 60 seconds = 6
+//write some text if countdown is over
+if(distance < 0){
+    clearInterval(x);
+    document.getElementById("time").style.display = 'none';
+    document.querySelector(".newYear").style.display = 'block';
+}
+})
