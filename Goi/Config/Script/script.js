8fd56a00937a192b5a97bb2649b7926fa0ad6e4e
KhaiTQ10 <khaiquangtranabc@gmail.com>
2024-12-14 17:23:12 +0700

    add voice

--- Goi/Config/Script/script.js
+++ Goi/Config/Script/script.js
@@ -1,7 +1,20 @@
 document.querySelectorAll(".card").forEach(function (card) {
   card.addEventListener("click", function () {
     this.classList.toggle("card1");
+    var japanText = card.getElementsByClassName("textJapan")[0].textContent;
+    console.log(japanText);
+    try {
+      const speech = new SpeechSynthesisUtterance();
+      speech.text = japanText;
+      speech.lang = "ja";
+      window.speechSynthesis.speak(speech);
+    }
+    catch (error) {
+      console.error("Error in translation:", error);
+      alert("The text could not be translated. Please try again.");
+    }
   });
+
 });
 
 import { images } from './../../../Home/images.js';
