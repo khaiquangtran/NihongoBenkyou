b2e148043ef1b2fffd4a895d2886d5e4b2fe3469
KhaiTQ10 <khaiquangtranabc@gmail.com>
2025-04-27 12:00:13 +0700

    add lesson tuvung 22

--- Goi/Config/Script/script.js
+++ Goi/Config/Script/script.js
@@ -5,10 +5,8 @@ document.querySelectorAll(".card").forEach(function (card) {
       var japanText = card.getElementsByClassName("textJapan")[0].textContent;
       // console.log(japanText);
       try {
-        const speech = new SpeechSynthesisUtterance();
-        speech.text = japanText;
-        speech.lang = "ja";
-        window.speechSynthesis.speak(speech);
+        const content = localStorage.getItem('content');
+        speak(content, japanText)
       }
       catch (error) {
         console.error("Error in translation:", error);
@@ -27,3 +25,37 @@ var number = Math.floor(Math.random() * imgCount);
 window.onload = function () {
   randomChange.style.backgroundImage = "url(" + images[number] + ")";
 };
+
+async function speak(keyConnect, text) {
+  const response = await fetch(`https://texttospeech.googleapis.com/v1/text:synthesize?key=${keyConnect}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      input: { text: text },
+      voice: {
+        languageCode: 'ja-JP',
+        name: 'ja-JP-Standard-A' // can use A, B, C...
+      },
+      audioConfig: {
+        audioEncoding: 'MP3'
+      }
+    })
+  });
+
+  if (!response.ok) {
+    alert('❌ Please choose again KEY file');
+    return;
+    // data.audioContent là base64 của file MP3
+  }
+  const data = await response.json();
+
+  if (data.audioContent) {
+    const audio = new Audio("data:audio/mp3;base64," + data.audioContent);
+    audio.play();
+  } else {
+    console.error("Error audioContent", data);
+    alert("Can't play!");
+  }
+}
