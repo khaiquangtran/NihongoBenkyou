d079b08d831475102f88a93e262f786fefacca6d
KhaiTQ10 <khaiquangtranabc@gmail.com>
2025-07-04 06:03:17 +0700

    big update

---
+++ Tango/Goi/Config/Script/script.js
@@ -0,0 +1,108 @@
+document.querySelectorAll(".card").forEach(function (card) {
+  card.addEventListener("click", function () {
+    var check = this.classList.toggle("card1");
+    if (check) {
+      var japanText = card.getElementsByClassName("textJapan")[0].textContent;
+      // console.log(japanText);
+      try {
+        const content = localStorage.getItem('content');
+        speak(content, japanText)
+      }
+      catch (error) {
+        console.error("Error in translation:", error);
+        alert("The text could not be translated. Please try again.");
+      }
+    }
+  });
+
+});
+
+const images = [
+    "../../../../Home/Images/home_background.jpg",
+    "../../../../Home/Images/home_background1.jpg",
+    "../../../../Home/Images/home_background2.jpg",
+    "../../../../Home/Images/home_background3.jpg",
+    "../../../../Home/Images/home_background4.jpg",
+    "../../../../Home/Images/home_background5.jpg",
+    "../../../../Home/Images/home_background6.jpg",
+    "../../../../Home/Images/home_background7.jpg",
+    "../../../../Home/Images/home_background8.jpg",
+    "../../../../Home/Images/home_background9.jpg",
+    "../../../../Home/Images/home_background10.jpg",
+    "../../../../Home/Images/home_background11.jpg",
+    "../../../../Home/Images/home_background12.jpg",
+    "../../../../Home/Images/home_background13.jpg",
+    "../../../../Home/Images/home_background14.jpg",
+    "../../../../Home/Images/home_background15.jpg",
+    "../../../../Home/Images/home_background16.jpg",
+    "../../../../Home/Images/home_background17.jpg",
+    "../../../../Home/Images/home_background18.jpg",
+    "../../../../Home/Images/home_background19.jpg",
+    "../../../../Home/Images/home_background20.jpg",
+    "../../../../Home/Images/home_background21.jpg",
+    "../../../../Home/Images/home_background22.jpg",
+    "../../../../Home/Images/background23.jpg",
+    "../../../../Home/Images/background24.jpg",
+    "../../../../Home/Images/background25.jpg",
+    "../../../../Home/Images/background26.jpg",
+    "../../../../Home/Images/background27.jpg",
+    "../../../../Home/Images/background28.jpg",
+  ];
+var randomChange = document.getElementById("Background");
+var imgCount = images.length;
+var number = Math.floor(Math.random() * imgCount);
+
+window.onload = function () {
+  randomChange.style.backgroundImage = "url(" + images[number] + ")";
+};
+
+async function speak(keyConnect, text) {
+  const response = await fetch(`https://texttospeech.googleapis.com/v1/text:synthesize?key=${keyConnect}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      input: { text: text },
+      voice: {
+        languageCode: 'ja-JP',
+        name: 'ja-JP-Standard-A' // can use A, B, C...
+      },
+      audioConfig: {
+        audioEncoding: 'MP3'
+      }
+    })
+  });
+
+  if (!response.ok) {
+    alert('❌ Please choose again KEY file');
+    return;
+    // data.audioContent là base64 của file MP3
+  }
+  const data = await response.json();
+
+  if (data.audioContent) {
+    const audio = new Audio("data:audio/mp3;base64," + data.audioContent);
+    audio.play();
+  } else {
+    console.error("Error audioContent", data);
+    alert("Can't play!");
+  }
+}
+
+function randomSlides() {
+  let container = document.getElementById("wrapper");
+  let slides = Array.from(container.children);
+
+  slides.sort(() => Math.random() - 0.5);
+
+  const fragment = document.createDocumentFragment();
+  slides.forEach((slide) => fragment.appendChild(slide));
+
+  container.innerHTML = "";
+  container.appendChild(fragment);
+
+  // swiper.update();
+}
+
+document.getElementById("shuffleBtn").addEventListener("click", randomSlides);
--- Tool/getTimePoint.py
+++ Tool/getTimePoint.py
@@ -1,30 +1,30 @@
 from google.cloud.texttospeech_v1beta1 import VoiceSelectionParams, AudioConfig, AudioEncoding, SynthesizeSpeechRequest, SynthesisInput, TextToSpeechClient
 import os
 
-# Set biến môi trường để dùng credentials (từ file JSON tải từ Google Cloud)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "H:/13_Tai_lieu/virtual.json"  # 🔁 thay đường dẫn này
 
 def synthesize():
     client = TextToSpeechClient()
 
-    synthesis_input = SynthesisInput(ssml="""
-<speak>
-    <mark name="1"/>日本へ
-    <mark name="2"/>勉強に
-    <mark name="3"/>行きます。
-    <mark name="4"/>あなたは
-    <mark name="5"/>どこへ
-    <mark name="6"/>行きますか。
-    <mark name="7"/>私は
-    <mark name="8"/>日本へ
-    <mark name="9"/>行きます。
-    <mark name="10"/>あなたは
-    <mark name="11"/>日本へ
-    <mark name="12"/>何を
-    <mark name="13"/>しに
-    <mark name="14"/>行きますか。
-</speak>
-""")
+#     synthesis_input = SynthesisInput(ssml="""
+# <speak>
+#     <mark name="1"/>日本へ
+#     <mark name="2"/>勉強に
+#     <mark name="3"/>行きます。
+#     <mark name="4"/>あなたは
+#     <mark name="5"/>どこへ
+#     <mark name="6"/>行きますか。
+#     <mark name="7"/>私は
+#     <mark name="8"/>日本へ
+#     <mark name="9"/>行きます。
+#     <mark name="10"/>あなたは
+#     <mark name="11"/>日本へ
+#     <mark name="12"/>何を
+#     <mark name="13"/>しに
+#     <mark name="14"/>行きますか。
+# </speak>
+# """)
+    synthesis_input = SynthesisInput(text="""次は、岩手県です。岩手県は、東北地方にある県です。面積がとても広くて、日本で２番目に大きい県です。岩手県は、漁業や農業が盛んです。海では、こんぶなどの海藻がよくとれます。岩手の料理といえば、「わんこそば」が有名です。小さな器に、少しずつそばを入れて食べる料理です。また、岩手では、盛岡冷麺も有名です。暑い夏に冷たい麺を食べるのが、岩手の習慣です。岩手では、毎年夏に「さんさ踊り」という祭りがあります。太鼓の音に合わせて、たくさんの人が踊ります。""")
     voice = VoiceSelectionParams(
         language_code="ja-JP",
         name="ja-JP-Neural2-B",
